8be8fd5ff9f697f96d5b293160f89d4e303c46b0
Brenthubbard <76464105+Brenthubbard@users.noreply.github.com>
2021-06-09 12:09:54 -0600

    add rest of file tree

---
+++ CretaceousClient/Controllers/AnimalsController.cs
@@ -0,0 +1,15 @@
+using CretaceousClient.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CretaceousClient.Controllers
+{
+  public class AnimalController : Controller
+{
+  public IActionResult Index()
+  {
+      var allAnimals = Animal.GetAnimals();
+      return View(allAnimals);
+    }
+}
+}
---
+++ CretaceousClient/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CretaceousClient.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+    
+}
